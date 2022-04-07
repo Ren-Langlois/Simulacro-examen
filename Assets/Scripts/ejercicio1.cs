2c2b23f59d66b1de3633f6a1c55b94dc8781b3a4
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-04-07 13:52:27 -0300

    Initial commit

---
+++ Assets/Scripts/ejercicio1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+public class ejercicio1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los números son iguales");
+        }
+        else
+        {
+            Debug.Log("Los números no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ejercicio2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos 
+//y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+//Luego indicar cuánto dinero sobra o falta.
+
+public class ejercicio2 : MonoBehaviour
+{
+    public int producto1;
+    public int producto2;
+    public int producto3;
+    public int presupuesto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int preciototal = producto1 + producto2 + producto3;
+        if (preciototal > presupuesto)
+        {
+            Debug.Log("El precio de los productos supera el presupuesto");
+            int falta = preciototal -  presupuesto;
+            Debug.Log("sobra " + falta);
+        }
+        else 
+        {
+            Debug.Log("El precio de los productos no supera el presupuesto");
+            int sobra = presupuesto - preciototal;
+            Debug.Log("falta " + sobra);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
